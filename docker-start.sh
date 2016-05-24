f14e03473eeb744a74569b48a4f87cbb677682a0
Philipp Wagner <philipp.wagner@incowia.com>
2016-05-24 16:30:43 +0200

    PLAT-479: adjust docker scripts

--- docker-start.sh
+++ docker-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker start cubx-wp-db && docker start cubx-wp
+docker start cubx-wp-db && docker start cubx-wp && docker start cubx-wp-myadmin
--- docker-stop.sh
+++ docker-stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker stop cubx-wp && docker stop cubx-wp-db
+docker stop cubx-wp && docker stop cubx-wp-db && docker stop cubx-wp-myadmin
